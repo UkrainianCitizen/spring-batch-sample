6dedcffe1425c2b7d8757d52b197c01796a7997d
Alexander Huba <oleksandr.huba@open.ac.uk>
2019-08-05 16:34:43 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring Batch Service
+
+A service that imports data from a CSV spreadsheet, 
+transforms it with custom code, and stores the final 
+results in a database.
---
+++ src/main/java/org/ukrcitizen/gsbatchprocessing/GsBatchProcessingApplication.java
@@ -0,0 +1,13 @@
+package org.ukrcitizen.gsbatchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsBatchProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsBatchProcessingApplication.class, args);
+    }
+
+}
